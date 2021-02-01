f3516e81e82a1e2d09510c742da3155090cd033b
Poul <poulbachlauritsen@gmail.com>
2021-02-01 12:14:45 +0100

    Lille update cleanup

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-
-    var target = Math.floor(Math.random() * 10 + 1); 
-        
-    var NumOfGuesses = 1; 
+var target = Math.floor(Math.random() * 10 + 1); 
+var NumOfGuesses = 1; 
         
 const submit = () => {
-
-
     var guess = document.getElementById("submitField").value;
 
-    if(guess == target) 
-    {     
+    if(guess == target) {     
         alert("Tilykke. Du gættede det rigtige tal på " + NumOfGuesses + " gæt");  
     } 
-    else if(guess > target) /* if guessed number is greater 
-                    than actual number*/ 
-    {     
+    else if(guess > target) {   
         NumOfGuesses++; 
         alert("Gættet var for højt"); 
-    } 
-    else
-    { 
+    }
+    else { 
         NumOfGuesses++; 
         alert("Gættet var for lavt");
     } 
